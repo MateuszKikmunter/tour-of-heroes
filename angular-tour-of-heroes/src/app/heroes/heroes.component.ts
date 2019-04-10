8bbcb38bb1ddd705a9a9f43e4ec5a63cc9cea03c
Mateusz Kikmunter <mateusz.kikmunter@willistowerswatson.com>
2019-04-10 13:44:52 +0100

    first module completed

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,3 +1,4 @@
+import { Hero } from './../hero';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  public hero: string = "windstorm";
+  public hero: Hero = {
+    id: 1,
+    name: "Windstorm"
+  };
 
   constructor() { }
 

7cbe0f5b58f798fd76cc278447850ca09ed20a3e
Mateusz Kikmunter <mateusz.kikmunter@willistowerswatson.com>
2019-04-11 10:53:10 +0100

    implemented search

--- angular-tour-of-heroes/src/app/hero-search/hero-search.component.ts
+++ angular-tour-of-heroes/src/app/hero-search/hero-search.component.ts
@@ -1,5 +1,9 @@
-import { HeroService } from './../hero.service';
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+
+import { HeroService } from './../hero.service';
+import { Hero } from '../hero';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-hero-search',
@@ -8,11 +12,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroSearchComponent implements OnInit {
 
+  heroes$: Observable<Hero[]>;
+  private searchTerms = new Subject<string>();
+
   constructor(public heroService: HeroService) { }
 
   ngOnInit() {
+    this.heroes$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: string) => this.heroService.searchHeroes(term))
+    );
   }
 
-  
+  public search(term: string): void {
+    this.searchTerms.next(term);
+  }
 
 }
